2c3f55d5a2ff255eea2602a1ece74727085865ff
Brian Zheng <brianzheng205@gmail.com>
2024-10-02 20:28:42 -0400

    Add methods to Associating

--- server/concepts/associating.ts
+++ server/concepts/associating.ts
@@ -2,8 +2,8 @@ import { ObjectId } from "mongodb";
 import DocCollection, { BaseDoc } from "../framework/doc";
 
 export interface ItemDoc extends BaseDoc {
-  item: ObjectId;
   author: ObjectId;
+  item: ObjectId;
 }
 
 /**
@@ -17,12 +17,21 @@ export default class AssociatingConcept {
   }
 
   async associate(item: ObjectId, author: ObjectId) {
-    const _id = await this.owner.createOne({ item, author });
+    const _id = await this.owner.createOne({ author, item });
     return { msg: "Association successfully created!", item: await this.owner.readOne({ _id }) };
   }
 
   async disassociate(item: ObjectId) {
     await this.owner.deleteOne({ item });
     return { msg: "Association deleted successfully!" };
+  }
+
+  async getByAuthor(author: ObjectId) {
+    return this.owner.readMany({ author });
+  }
+
+  async getAuthor(item: ObjectId) {
+    const itemDoc = await this.owner.readOne({ item });
+    return itemDoc ? itemDoc.author : null;
   }
 }
